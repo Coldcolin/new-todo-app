338fda636b8f2f0c4cb98fb76d3450c01c70dcce
Coldcolin <decorcecolin@gmail.com>
2022-12-12 09:26:39 +0100

    added features

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
 import Card  from './Card.jsx';
-import React, {useRef, useState} from "react";
+import React, {useRef, useState, useReducer, useEffect} from "react";
 import "./App.css";
-// import Card from './Card.jsx'
+
+const initialState=[];
+
+const reducer=(todo, actions)=>{
+  switch(actions.type){
+    case("create"):
+      return [...todo, { id: actions.payload.id + 1, title: actions.payload.title, checked: false}]
+    case("toggle"):
+      return todo.map((i)=> i.id === actions.payload.id? {...i, checked: !i.checked}: {...i})
+    default:
+      return todo
+  }
+}
+
+
 function App() {
   const [val, setVal] = useState("")
+  const [todo, dispatch] = useReducer(reducer, initialState)
   const inputRef = useRef('')
 
   const handleChange=()=>{
     setVal(inputRef.current.value)
   }
 
+  const addTodo=()=>{
+    dispatch({type: "create", payload:{id: todo.length ,title: val}})
+  }
+  // useEffect(() => {
+  //   console.log(todo)
+  // }, [todo])
+  
 
   return (
     <div className="App">
       <div className="app-wrap2">
         <div className="todo">
           <div className="input-holder">
-            <input ref={inputRef} className="place" placeholder="Add new todo.." />
-            <button onClick={handleChange} className="add">+</button>
+            <input ref={inputRef} className="place" placeholder="Add new todo.." onChange={handleChange}/>
+            <button onClick={addTodo} className="add">+</button>
           </div>
         </div>
-        <Card input={val}    />
+        <Card todo={todo} dispatch={dispatch} />
       </div>
     </div>
   );
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,31 +1,42 @@
+import React, {useEffect, useState} from "react";
 
-import React, {useRef} from "react";
-export default function Card({input}) {
+export default function Card({todo, dispatch}) {
+  const [count, setCounter] = useState([]);
 
+  const completeCheck=()=>{
+    const complete = todo?.filter((i)=> i.checked === true );
+    setCounter(complete)
+  }
 
+  useEffect(()=>{
+    completeCheck()
+  },[todo])
 
-  return <div className="card_holder">
-          <div className="top">
-            <h1>Todo's</h1>
-            <p>3 Total 2 complete and 1 pending</p>
+  return (
+      <div className="card_holder">
+        <div className="top">
+          <h1>Todo's</h1>
+          <p>{todo.length} Total {count.length} complete and {todo.length - count.length} pending</p>
+        </div>
+        <div className="card">
+          <div className="heading">
+            <p>#</p>
+            <h3>Todo Title</h3>
+            <h3>Status</h3>
           </div>
-          <div className="card">
-            <div className="heading">
-              <p>#</p>
-              <h3>Todo Title</h3>
-              <h3>Status</h3>
-            </div>
-            <div className="tasks">
-              <input type="checkbox" />
-              <p>Todo Title</p>
-              <p>Status</p>
-            </div>
-            <div className="tasks1">
-              <input type="checkbox" />
-              <p>{input}</p>
-              <p>Status</p>
-            </div>
+          <div className="task-holder">
+            {
+              todo?.map((item)=>(
+                <div key={item.id} className="tasks1" style={{backgroundColor: item.checked? "#98e975": "#fff"}}>
+                  <input type="checkbox" onChange={()=>{dispatch({type: "toggle", payload:{id: item.id}})}}/>
+                  <p className="todo-text">{item.title}</p>
+                  {<p>{item.checked? "completed": "pending"}</p>}
+                </div>
+              ))
+            }
           </div>
-        </div>;
+        </div>
+      </div>
+  )
 }
   
